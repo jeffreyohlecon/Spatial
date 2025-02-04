29bbb2ccf6e435c5a11a19db71c6f0ee08c61ae4
Henrique Mota <henriquerodriguesmota@gmail.com>
2025-02-04 08:11:16 -0600

    New figures, new code for generating maps separetely.

--- code/counterfactual_exercises.jl
+++ code/counterfactual_exercises.jl
@@ -1,10 +1,11 @@
 using Pkg
 using CSV, DataFrames, LinearAlgebra, Statistics, SparseArrays, Plots, StatsPlots, ColorSchemes, StatsBase
+using CategoricalArrays
 
 #Extracting some stuff from Dropbox before setting up the CD
 
 #d = Matrix( CSV.read("/Users/henriquemota/Dropbox/BigDataFiles/d_ni.csv", DataFrame; header=false) )
-hatB  =   Matrix( select( CSV.read("/Users/henriquemota/Dropbox/BigDataFiles/B_ni_hat.csv", DataFrame), Not(:FIPS) ) )
+hatB  =   Matrix( select( CSV.read("/Users/henriquemota/Dropbox/BigDataFiles/B_ni_hat_destination.csv", DataFrame), Not(:FIPS) ) )
 π = Matrix(  select( CSV.read("/Users/henriquemota/Dropbox/BigDataFiles/pi_ni.csv", DataFrame), Not(:state_county_code) ) )
 π = sparse(π)
 
@@ -262,6 +263,19 @@ sum((cf_hatQ ./ cf_hatP  .>1) .*  R ) ./sum(R)
 
 # Create a DataFrame with the required columns
 
+# Preparing Dataframe for Plots
+result_df = DataFrame(
+    vector_number = vector_number,
+    old_w = (old_w .-1) .*100, 
+    cf_hatL = (cf_hatL .-1).*100, 
+    cf_hatR = (cf_hatR .-1).*100, 
+    diag_old_λ = (diag(old_λ) .-1).*100,
+    real_v = (real_v .-1).*100
+)
+
+# Shocks for the maps 
+CSV.write("output/map_database.csv", result_df)
+
 result_df_main = DataFrame(
     vector_number = vector_number,
     hat_w = old_w,
@@ -271,12 +285,82 @@ result_df_main = DataFrame(
     diag_hat_λ = diag(old_λ), 
     hatP = cf_hatP,
     hatQ = cf_hatQ,
-    real_v = real_v)
+    real_v = real_v, 
+    L = L, 
+    R = R)
 
 # Save the result_df as a .csv file in output as result_first_cf
 CSV.write("output/result_first_cf.csv", result_df_main)
 
+# Calculate percentiles for L and R
+percentiles_L = quantile(L, 0:0.05:1)
+percentiles_R = quantile(R, 0:0.05:1)
 
+percentiles_L[21] = percentiles_L[21] + 1e-6
+percentiles_R[21] = percentiles_R[21] + 1e-6
 
+# Assign percentiles to each county
+result_df_main[!, :percentile_L] = cut(L, percentiles_L, labels=1:20)
+result_df_main[!, :percentile_R] = cut(R, percentiles_R, labels=1:20)
 
+# Calculate means of hat_L and hat_R for each percentile
+result_df_main[!, :adj_hatL] = (result_df_main[!, :hatL] .- 1) .* 100
+result_df_main[!, :adj_hatR] = (result_df_main[!, :hatR] .- 1) .* 100
+
+mean_hatL_by_percentile_L = combine(groupby(result_df_main, :percentile_L)) do df
+    w = Weights(df.L)
+    (; weighted_mean_adj_hatL = mean(df.adj_hatL, w))
+end
+
+# Compute weighted mean of adj_hatR using R as weight
+mean_hatR_by_percentile_R = combine(groupby(result_df_main, :percentile_R)) do df
+    w = Weights(df.R)
+    (; weighted_mean_adj_hatR = mean(df.adj_hatR, w))
+end
+
+# Generate bar plots for weighted mean_hatL based on percentiles of L
+bar_plot_weighted_L = @df mean_hatL_by_percentile_L bar(:percentile_L, :weighted_mean_adj_hatL, legend=false, xlabel="Percentiles of L", ylabel="Weighted Mean Growth", title="Weighted Mean Growth by Percentiles of L")
+
+# Generate bar plots for weighted mean_hatR based on percentiles of R
+bar_plot_weighted_R = @df mean_hatR_by_percentile_R bar(:percentile_R, :weighted_mean_adj_hatR, legend=false, xlabel="Percentiles of R", ylabel="Weighted Mean Growth", title="Weighted Mean Growth by Percentiles of R")
+
+# Save the plots
+savefig(bar_plot_weighted_L, "output/figures/weighted_hat_L_by_percentiles_of_L.png")
+savefig(bar_plot_weighted_R, "output/figures/weighted_hat_R_by_percentiles_of_R.png")
+
+# Calculate deciles for L and R
+deciles_L = quantile(L, 0:0.1:1)
+deciles_R = quantile(R, 0:0.1:1)
+
+deciles_L[11] = deciles_L[11] + 1e-6
+deciles_R[11] = deciles_R[11] + 1e-6
+
+# Assign deciles to each county
+result_df_main[!, :decile_L] = cut(L, deciles_L, labels=1:10)
+result_df_main[!, :decile_R] = cut(R, deciles_R, labels=1:10)
+
+# Calculate means of hat_L and hat_R for each decile
+result_df_main[!, :adj_hatL] = (result_df_main[!, :hatL] .- 1) .* 100
+result_df_main[!, :adj_hatR] = (result_df_main[!, :hatR] .- 1) .* 100
+
+# Calculate means of hat_L and hat_R for each decile
+mean_hatL_by_decile_L = combine(groupby(result_df_main, :decile_L)) do df
+    w = Weights(df.L)
+    (; weighted_mean_adj_hatL = mean(df.adj_hatL, w))
+end
+
+mean_hatR_by_decile_R = combine(groupby(result_df_main, :decile_R)) do df
+    w = Weights(df.R)
+    (; weighted_mean_adj_hatR = mean(df.adj_hatR, w))
+end
+
+# Generate bar plots for weighted mean_hatL based on deciles of L
+bar_plot_weighted_L_decile = @df mean_hatL_by_decile_L bar(:decile_L, :weighted_mean_adj_hatL, legend=false, xlabel="Deciles of L", ylabel="Weighted Mean Growth", title="Weighted Mean Growth by Deciles of L")
+
+# Generate bar plots for weighted mean_hatR based on deciles of R
+bar_plot_weighted_R_decile = @df mean_hatR_by_decile_R bar(:decile_R, :weighted_mean_adj_hatR, legend=false, xlabel="Deciles of R", ylabel="Weighted Mean Growth", title="Weighted Mean Growth by Deciles of R")
+
+# Save the plots
+savefig(bar_plot_weighted_L_decile, "output/figures/weighted_hat_L_by_deciles_of_L.png")
+savefig(bar_plot_weighted_R_decile, "output/figures/weighted_hat_R_by_deciles_of_R.png")
 
--- code/plot_figures.jl
+++ code/plot_figures.jl
@@ -1,18 +1,19 @@
+# Pkg.add(["Shapefile", "DataFrames", "FilePathsBase", "Plots", "CSV", "Missings"])
+
 using Shapefile
 using DataFrames
 using Plots
+using CSV 
+import .Missings 
 
-# Preparing Dataframe for Plots
-result_df = DataFrame(
-    vector_number = vector_number,
-    old_w = (old_w .-1) .*100, 
-    cf_hatL = (cf_hatL .-1).*100, 
-    cf_hatR = (cf_hatR .-1).*100, 
-    diag_old_λ = (diag(old_λ) .-1).*100
-)
+# Define the shapefile path in a way that is adaptable to any computer
+shapefile_path = joinpath(homedir(), "Dropbox", "BigDataFiles", "2010_county_shape_file", "tl_2010_us_county10.shp") 
 
-# Load the shapefile
-shapefile_path = "/Users/henriquemota/Dropbox/BigDataFiles/2010_county_shape_file/tl_2010_us_county10.shp"
+# Define your Directory here 
+cd("/Users/henriquemota/Library/CloudStorage/OneDrive-Personal/Documentos/_PhD_Classes/Trade Track/Spatial/Programming/")
+
+# Load the CSV file into a DataFrame
+result_df = DataFrame(CSV.File("output/map_database.csv"))
 shapes = Shapefile.Table(shapefile_path)
 
 # Convert the shapefile to a DataFrame
@@ -20,12 +21,10 @@ shape_df = DataFrame(shapes)
 
 # Ensure vector_number has 5 digits by adding leading zeros if necessary
 # Create a new vector with 5-digit FIPS codes
-result_df.vector_number_5digit = lpad.(string.(vector_number), 5, '0')
+result_df.vector_number_5digit = lpad.(string.(result_df.vector_number), 5, '0')
 
 # Merge the result_df with the shape_df on the FIPS code
 merged_df = leftjoin(shape_df, result_df, on = :GEOID10 => :vector_number_5digit)
-
-using Plots
 
 default(dpi = 900)
 
@@ -37,18 +36,23 @@ ymin, ymax = 24, 50
 function plot_variable(data, variable_name, title_name)
 
     p = plot(color=:blues, grid=false, framestyle=:none, axis=nothing, ratio=1, 
-             xlims=(xmin, xmax), ylims=(ymin, ymax), title=title_name, size = (900,675), colorbar_size=0.005)
+             xlims=(xmin, xmax), ylims=(ymin, ymax), title=title_name, size = (900,675))
 
             
 min_val = minimum(skipmissing(data[!,variable_name]))
 max_val = maximum(skipmissing(data[!,variable_name]))
 
-# Define the custom 5-color scale
-custom_cmap = cgrad([:red, :yellow, :green],  scale=:symlog) 
+val = maximum([abs(min_val), abs(max_val)]) 
+
+clims=(-1.05, 1.05).*val
+
+# Define the custom 3-color scale with min_val, 0, and max_val
+custom_cmap = cgrad([:red, :white, :green], scale=:symlog, rev=false)
 
     for row in eachrow(data)
         if !ismissing(row[variable_name])
-            plot!(p, row.geometry, fill_z=row[variable_name], label="", lw=0.5, linecolor=:black, c=custom_cmap)
+           # plot!(p, row.geometry, fill_z=row[variable_name], label="", lw=0.5, linecolor=:black, c=custom_cmap, clims=clims, colorbar_size=0.03, colorbar_title="", colorbar_titlefont=font(8))
+            plot!(p, row.geometry, fill_z=row[variable_name], label="", lw=0.5, linecolor=:black, c=custom_cmap, clims = clims, colorbar_size=0.03)
         end
     end
 
@@ -56,15 +60,17 @@ custom_cmap = cgrad([:red, :yellow, :green],  scale=:symlog)
 end
 
 # Generate maps for the four variables
+# Use the PlotlyJS backend for interactive plots
 p1 = plot_variable(merged_df, :old_w, "Old Wages by County")
 p2 = plot_variable(merged_df, :cf_hatL, "Change in Workers by County")
 p3 = plot_variable(merged_df, :cf_hatR, "Change in Residents by County")
 p4 = plot_variable(merged_df, :diag_old_λ, "Change in Living Where You Work by County")
-
+p5 = plot_variable(merged_df, :real_v, "Change in Real Average Income by County")
 
 # Save the plots to the output/figures folder with higher resolution
 savefig(p1, "output/figures/old_wages_by_county.png")
 savefig(p2, "output/figures/change_in_workers_by_county.png")
 savefig(p3, "output/figures/change_in_residents_by_county.png")
 savefig(p4, "output/figures/change_in_living_where_you_work_by_county.png")
+savefig(p5, "output/figures/change_in_real_income.png")
 
